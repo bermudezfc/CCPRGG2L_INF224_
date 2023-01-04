327ab36055b3d5b55c6daf0fc158dfc604762f9d
Fortune Raphael C. Bermudez <111546354+bermudezfc@users.noreply.github.com>
2023-01-05 02:26:16 +0800

    Create Task4.java

---
+++ LabAct/Task4.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Task4 {
+
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+                // Scanner declaration
+		Scanner scan = new Scanner(System.in);
+		
+                // User input of chosen file name
+		System.out.print("Enter file name: "); 
+		String fileName = scan.next();
+                
+                   
+                // File path/directory
+		File inputFile = new File("C:\\Users\\Gail\\Documents\\NetBeansProjects\\Programming2Activity\\dummy.txt"); //tinatamad na ko mag comment...
+		String[] words = null;
+                // Initial value for word count
+		int wordCounter = 0;
+		FileReader fr = new FileReader(inputFile);
+		BufferedReader br = new BufferedReader(fr);
+		String w;
+		
+		try {
+			while((w = br.readLine())!= null) {
+			words = w.split(" ");
+                            wordCounter = wordCounter + words.length;
+		}
+		fr.close();
+		System.out.println("Word count for this file: "+wordCounter);
+		}
+		catch(Exception e) {
+			System.out.println("Error: "+e.getMessage());
+                }
+
+		}
+	
+	}
+
